d8de6286d024f3832611a1209d87875ebaf5f59b
Hagen Tönnies <hagen.toennies@geektanklabs.de>
2014-07-08 09:00:29 +0200

    Adds test ng.

---
+++ src/test/java/org/elasticsearch/river/wikipedia/support/WikiTextParserTest.java
@@ -0,0 +1,11 @@
+package org.elasticsearch.river.wikipedia.support;
+
+import static org.testng.Assert.*;
+
+public class WikiTextParserTest {
+
+  @org.testng.annotations.Test
+  public void testGetText() throws Exception {
+
+  }
+}
